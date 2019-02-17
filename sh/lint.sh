6b85d57830571933ccb69c571cc30a226819cf8e
Phil Cockfield <phil@cockfield.net>
2019-02-18 09:13:32 +1300

    update refs

--- sh/lint.sh
+++ sh/lint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 # Format code.
-PRETTIER_CONFIG=$PWD/node_modules/@tdb/typescript/.prettierrc
-cp $PRETTIER_CONFIG $PWD/.prettierrc
 node ./node_modules/prettier/bin-prettier --write 'code/src/**/*.ts{,x}'
 
 # Lint.
