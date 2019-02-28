9b465b3f36403eee2e7875551105706c4cb1b79a
Phil Cockfield <phil@cockfield.net>
2019-03-01 09:12:49 +1300

    fix call sites

--- src/common/util.npm.ts
+++ src/common/util.npm.ts
@@ -1,4 +1,4 @@
-import { exec, log } from './libs';
+import { npm } from './libs';
 import { compact } from './util';
 import { IModule, INpmInfo } from '../types';
 
@@ -7,48 +7,6 @@ import { IModule, INpmInfo } from '../types';
  */
 export async function info(pkg: IModule | IModule[]) {
   const modules = (Array.isArray(pkg) ? pkg : [pkg]).filter(pkg => pkg.json.private !== true);
-
-  const items = await Promise.all(modules.map(item => getInfo(item)));
+  const items = await Promise.all(modules.map(item => npm.getInfo(item.name)));
   return compact(items) as INpmInfo[];
-}
-
-async function getInfo(pkg: IModule): Promise<INpmInfo | undefined> {
-  const cmd = `npm info ${pkg.name} --json`;
-
-  const parseJson = (text: string) => {
-    try {
-      const json = JSON.parse(text);
-      return json;
-    } catch (error) {
-      log.error('Raw JSON text:');
-      log.info(text);
-      throw error;
-    }
-  };
-
-  try {
-    const result = await exec.cmd.run(cmd, { silent: true });
-    const stdout = result.info.join('\n');
-    if (!stdout) {
-      return undefined;
-    }
-    const json = parseJson(stdout);
-    const latest = json['dist-tags'].latest;
-    const name = json.name;
-    return {
-      name,
-      latest,
-      json,
-    };
-  } catch (error) {
-    if (error.message.includes('Not found')) {
-      return undefined; // Return nothing indicating the module was not found on NPM.
-    } else {
-      throw new Error(
-        `Failed while reading info for '${pkg.name}' from NPM.\nCMD: ${log.yellow(cmd)}\n\n${
-          error.message
-        }`,
-      );
-    }
-  }
 }
