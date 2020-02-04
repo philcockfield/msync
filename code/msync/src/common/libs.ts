d628065f39566dc2ede9efde96a52fe0efd53eaf
Phil Cockfield <phil@cockfield.net>
2020-02-04 14:37:06 +1300

    refs

--- code/msync/src/common/libs.ts
+++ code/msync/src/common/libs.ts
@@ -1,10 +1,7 @@
-import * as semver from 'semver';
-export { semver };
-
 export { log } from '@platform/log/lib/server';
 export { exec } from '@platform/exec';
 export { value, time, defaultValue } from '@platform/util.value';
-export { npm } from '@platform/npm';
+export { npm, semver } from '@platform/npm';
 export { fs } from '@platform/fs';
 
 export { moment, file, listr, IListrOptions, inquirer, plural } from 'command-interface';
